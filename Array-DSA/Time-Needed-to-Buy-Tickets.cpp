92e0ad539a119c6fc7aeb4a8a0b7eeba3cc0c1aa
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-02-25 09:38:00 +0530

    Implement timeRequiredToBuy function for ticket buying

---
+++ Array-DSA/Time-Needed-to-Buy-Tickets.cpp
@@ -0,0 +1,21 @@
+//LeetCode
+//2073. Time Needed to Buy Tickets -> Easy
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        
+        for (int i = 0; i < tickets.size(); i++) {
+            
+            if (i <= k) {
+                time += min(tickets[i], tickets[k]);
+            } 
+            else {
+                time += min(tickets[i], tickets[k] - 1);
+            }
+        }
+        
+        return time;
+    }
+};
