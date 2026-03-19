3c36850e0a0f5ffee0317f470651e7e443f06946
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-03-19 08:52:33 +0530

    Implement graph using adjacency list
    Added implementation for undirected unweighted graph using adjacency list.

--- Graph-DSA/Implementation-Using-Adjacency-List.cpp
+++ Graph-DSA/Implementation-Using-Adjacency-List.cpp
@@ -1,1 +1,38 @@
 //Implement Graph Using Adjency List
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph{
+    int node;
+public:
+    Graph(int node){
+        this->node = node;
+    }
+
+    vector<vector<int>>undirected_unweighted(vector<vector<int>>&vec){
+        vector<vector<int>> adj(node);
+
+        for(auto it : vec){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        return adj;
+    }
+};
+
+int main(){
+    Graph g(4);
+
+    vector<vector<int>>vec = {
+        {0, 1},
+        {1,2},
+        {1,3},
+        {2,3}
+    };
+
+    vector<vector<int>>gr = g.undirected_unweighted(vec);
+
+    return 0;
+}
