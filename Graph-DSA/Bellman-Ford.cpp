8dbb197065ffd2ec01d6447741be75f70bf8833b
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-03-31 00:29:33 +0530

    Refactor Bellman-Ford implementation for clarity

--- Graph-DSA/Bellman-Ford.cpp
+++ Graph-DSA/Bellman-Ford.cpp
@@ -1,35 +1,37 @@
 // GFG
 // Bellman-Ford -> Med.
 
-// User function Template for C++
-
 class Solution {
-    vector<vector<pair<int,int>>> createGraph(vector<vector<int>>&edge, int node){
-        vector<vector<pair<int, int>>>graph(node); //pair<int, int> child node and wt
-    
-        for(auto it : edge){
+    vector<vector<pair<int,int>>> createGraph(vector<vector<int>>& edge, int node) {
+        vector<vector<pair<int, int>>> graph(node);
+
+        for (auto it : edge) {
             graph[it[0]].push_back({it[1], it[2]});
         }
-    
+
         return graph;
     }
-  public:
+
+public:
     vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
-        // Code here
-        vector<vector<pair<int,int>>>graph = createGraph(edges, V);
-        vector<int>dist(V, INT_MAX);
+        vector<vector<pair<int,int>>> graph = createGraph(edges, V);
+
+        vector<int> dist(V, 1e8);
         dist[src] = 0;
-    
-        for(int i = 0; i < V-1; i++){
-            for(int u = 0; u < V; u++){
-                for(auto it : graph[u]){
-                    if(dist[it.first] > dist[u] + it.second){
-                        dist[it.first] = dist[u] + it.second;
+
+        for (int i = 0; i < V - 1; i++) {
+            for (int u = 0; u < V; u++) {
+                for (auto it : graph[u]) {
+                    int v = it.first;
+                    int wt = it.second;
+
+                    if (dist[u] != 1e8 && dist[v] > dist[u] + wt) {
+                        dist[v] = dist[u] + wt;
                     }
                 }
             }
         }
-    
+
         return dist;
     }
 };
