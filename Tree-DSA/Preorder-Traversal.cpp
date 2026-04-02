44a21134ba6700bd923e8db70a8226842f276dce
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-04-03 00:16:43 +0530

    Implement iterative preorder traversal method
    Added an iterative method for preorder traversal.

--- Tree-DSA/Preorder-Traversal.cpp
+++ Tree-DSA/Preorder-Traversal.cpp
@@ -16,12 +16,31 @@ class Node
 */
 
 class Solution {
-    void preOrders(Node* root, vector<int>&ans){
+    void preOrders(Node* root, vector<int>&ans){ //Recursive Code
         if(root == NULL) return;
         
         ans.push_back(root->data);
         preOrders(root->left, ans);
         preOrders(root->right, ans);
+    }
+
+    void preOrderIterative(Node* root){  //Iterative Code
+        stack<Node*>s;
+        while(true){
+            while(root){
+                cout<<root->data<<" ";
+    
+                s.push(root);
+                root = root->left;
+            }
+    
+            if(s.empty()){
+                break;
+            }
+            root = s.top();
+            s.pop();
+            root = root->right;
+        }
     }
   public:
     vector<int> preOrder(Node* root) {
