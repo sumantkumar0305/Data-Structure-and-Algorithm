7577568abcd1c747307c873fe4ae252d0d18623f
sumantkumar0305 <sumant.kumar0305@gmail.com>
2026-06-08 23:40:10 +0530

    Add Reverse-Integer.cpp file

---
+++ Math-DSA/Reverse-Integer.cpp
@@ -0,0 +1,22 @@
+// Leetcode
+// 7. Reverse Integer -> Med.
+
+class Solution {
+public:
+    int reverse(int x) {
+       long long ans = 0;
+
+        while(x != 0){
+            int digit = x%10;
+            ans = ans*10+digit;
+
+            if(ans > INT_MAX || ans < INT_MIN){
+                return 0;
+            }   
+
+            x /= 10;
+        }
+
+        return (int)ans;
+    }
+};
