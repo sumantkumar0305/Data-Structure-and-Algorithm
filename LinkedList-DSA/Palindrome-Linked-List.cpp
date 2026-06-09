fc3f526edeff6395f459130a2ffcade8b906ee0a
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-06-09 12:54:15 +0530

    Create Palindrome-Linked-List.cpp

---
+++ LinkedList-DSA/Palindrome-Linked-List.cpp
@@ -0,0 +1,31 @@
+// Leetocode
+// 234. Palindrome Linked List -> Easy
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        string str = "";
+
+        while(head != NULL){
+            str += to_string(head->val);
+
+            head = head->next;
+        }
+
+        string sttr = str;
+
+        reverse(str.begin(), str.end());
+
+        return sttr == str;
+    }
+};
