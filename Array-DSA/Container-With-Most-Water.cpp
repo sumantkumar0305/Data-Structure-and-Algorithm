990cc5079d20a4d562bda2d46652f22bbe7e7188
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-06-09 21:44:30 +0530

    Implement solution for Container With Most Water

---
+++ Array-DSA/Container-With-Most-Water.cpp
@@ -0,0 +1,21 @@
+// Leetcode
+// 11. Container With Most Water -> Med
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0; 
+        int right = height.size()-1;
+        int ans = INT_MIN;
+
+        while(left < right){
+            ans = max(ans, abs(left-right)*min(height[left], height[right]));
+
+            if(height[left] < height[right]){
+                left++;
+            }else right--;
+        }  
+
+        return ans;
+    }
+};
