927001abfe3024e3ba6748777234dc78164d412b
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-06-13 13:13:04 +0530

    Add deleteNode function for linked list
    Implement the deleteNode function to remove a node from a linked list by copying the value of the next node.

---
+++ LinkedList-DSA/Delete-Node-in-a-Linked-List.cpp
@@ -0,0 +1,23 @@
+// Leetcode
+// 237. Delete Node in a Linked List -> Med.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        while(node->next->next != NULL){
+            node->val = node->next->val;
+            node = node->next;
+        }
+
+        node->val = node->next->val;
+        node->next = NULL;
+    }
+};
