2d3b1cbf4645b98dda82c051b091b5397bb0700b
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-07-12 11:34:59 +0530

    Add solution for First Bad Version problem
    Implement binary search to find the first bad version.

---
+++ Array-DSA/First-Bad-Version.cpp
@@ -0,0 +1,19 @@
+// Leetcode
+// 278. First Bad Version -> Easy
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(isBadVersion(mid)) r = mid-1;
+            else l = mid+1;
+        }
+
+        return l;
+    }
+};
