d7b2bd5e35ff032b1590957d019e0722f8ec817a
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-07-14 22:15:09 +0530

    Implement Jump Game II solution in C++

---
+++ Array-DSA/Jump-Game-II.cpp
@@ -0,0 +1,22 @@
+// Leetcode
+// 45. Jump Game II -> Med.
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
