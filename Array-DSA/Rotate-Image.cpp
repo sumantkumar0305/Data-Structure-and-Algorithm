146981ca346eae4bf5de133f4363547fe028caa9
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-07-14 22:32:50 +0530

    Refactor rotate function for matrix rotation

--- Array-DSA/Rotate-Image.cpp
+++ Array-DSA/Rotate-Image.cpp
@@ -4,21 +4,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = i + 1; j < matrix.size(); j++) {
-                int val = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = val;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = i+1; j < matrix[0].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        int st = 0, end = matrix.size()-1;
-        while(st < end){
-            for(int i = 0; i < matrix.size(); i++){
-                swap(matrix[i][st], matrix[i][end]);
-            }
-            st++;
-            end--;
+        for(int i = 0; i < matrix.size(); i++){
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
