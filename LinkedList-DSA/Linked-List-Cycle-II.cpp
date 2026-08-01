197a0884a6c3078da22600acef385f06a900fd08
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-08-01 09:29:56 +0530

    Create Linked-List-Cycle-II.cpp

---
+++ LinkedList-DSA/Linked-List-Cycle-II.cpp
@@ -0,0 +1,36 @@
+// Leetcode
+// 142. Linked List Cycle II -> Med.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode* slow = head, *fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) break;
+        }
+
+        if(slow != fast) return NULL;
+
+        slow = head;
+        while(fast != NULL && slow != NULL){
+            if(slow == fast) return slow;
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return NULL;
+    }
+};
