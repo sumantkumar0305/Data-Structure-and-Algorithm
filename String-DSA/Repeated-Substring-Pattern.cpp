11943aa52462df57c5a451f6f715d569bbcf0a3f
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-08-06 18:15:20 +0530

    Add Repeated Substring Pattern solution
    Implement solution for detecting repeated substring patterns.

---
+++ String-DSA/Repeated-Substring-Pattern.cpp
@@ -0,0 +1,21 @@
+// Leetcode
+// 459. Repeated Substring Pattern -> Easy
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for(int i = 1; i < n; i++){
+            if(n%i == 0){
+                string str = s.substr(0, i);
+                string repeat = "";
+                for(int j = 0; j < n/i; j++){
+                    repeat += str;
+                }
+                if(repeat == s) return true;
+            }
+        }       
+
+        return false;
+    }
+};
