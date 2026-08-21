a2b3b0c455b08c654ac1c3d02588f3ca667d0e23
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-08-21 11:29:14 +0530

    Create Kth-Largest-Element-in-a-Stream.cpp

---
+++ System_Design-DSA/Kth-Largest-Element-in-a-Stream.cpp
@@ -0,0 +1,32 @@
+// Leetcode
+// 703. Kth Largest Element in a Stream -> Med.
+
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>>pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+
+        for(auto it : nums){
+            pq.push(it);
+        }
+
+        while(pq.size() > k){
+            pq.pop();
+        }
+    }
+
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > k)pq.pop();
+
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
