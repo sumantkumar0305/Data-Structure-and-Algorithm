218956c01c7f8a4c36f616a860f1eab38608b882
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-08-28 11:48:46 +0530

    Create Next-Permutation.cpp

---
+++ Array-DSA/Next-Permutation.cpp
@@ -0,0 +1,9 @@
+// Leetcode
+// 31. Next Permutation -> Med.
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
