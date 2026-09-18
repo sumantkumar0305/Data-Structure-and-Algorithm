303cf101ef00eaf4e676295547823ccb1325b991
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-09-18 13:28:33 +0530

    Update Rotate-String.cpp

--- String-DSA/Rotate-String.cpp
+++ String-DSA/Rotate-String.cpp
@@ -3,22 +3,16 @@
 
 
 class Solution {
-    string rotate(string str){
-        int st = 0, end = str.length()-1;
-
-        while(st != end){
-            swap(str[st], str[end]);
-            st++;
-        }
-
-        return str;
-    } 
 public:
     bool rotateString(string s, string goal) {
-        if(s == goal) return true;
-
+        if(s.length() != goal.length())return false;
+        if(s == goal)return true;
+        
         for(int i = 0; i < s.length(); i++){
-            s = rotate(s);
+            char ch = s[s.length()-1];
+            s = ch + s;
+            s.pop_back();
+
             if(s == goal)return true;
         }
 
